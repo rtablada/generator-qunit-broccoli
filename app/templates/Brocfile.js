2b2000f68d0eeaea85cd02759a04ca371586b1a2
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-10 23:06:31 -0500

    Update browserify reloading SASS

--- app/templates/Brocfile.js
+++ app/templates/Brocfile.js
@@ -9,6 +9,7 @@ const CssOptimizer = require('broccoli-csso');
 const Funnel = require('broccoli-funnel');
 const Babel = require('broccoli-babel-transpiler');
 const mv = require('broccoli-stew').mv;
+const rm = require('broccoli-stew').rm;
 const browserify = require('broccoli-browserify-cache');
 
 let pubFiles = new LiveReload('public');
@@ -21,8 +22,9 @@ const stylePaths = [
   'app/styles',
   'node_modules',
 ];
+const appNoSass = rm('app', '**/*.scss');
 
-const babelScript = new Babel('app');
+const babelScript = new Babel(appNoSass);
 
 const appScript = browserify(babelScript, {
   entries: ['./index'],
